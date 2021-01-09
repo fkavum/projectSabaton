682b13d593a74c5b8959755c1230e874ce42a25a
DESKTOP-6JDLMDB\fatih <fatihkavum@gmail.com>
2021-01-09 17:15:49 +0300

    Light detector

---
+++ Assets/scripts/AutoRotator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AutoRotator : MonoBehaviour
+{
+
+    public Vector3 endRotation;
+    public Ease easeType;
+    public float duration;
+    bool isRotating = false;
+    // Update is called once per frame
+    private void Start()
+    {
+        Rotate();
+    }
+
+    public void Rotate() {
+
+        if (isRotating) return;
+        isRotating = true;
+        
+        transform.DORotate(Vector3.zero, 0.2f).OnComplete(() =>
+        {
+            transform.DORotate(endRotation, duration).SetLoops(-1, LoopType.Yoyo).SetEase(easeType);
+        });
+    }
+
+    public void StopRotate()
+    {
+        if (!isRotating) return;
+        isRotating = false;
+        transform.DOKill();
+    }
+}
---
+++ Assets/scripts/LightArea.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class LightArea : MonoBehaviour
+{
+    public Light lightSource;
+    public AutoRotator rotator;
+    public float noSpotAngle;
+    public float yesSpotAngle;
+    public float radius;
+    public Vector3 center;
+    bool isCought = false;
+  
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(center,radius/2.5f);
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit, LayerMask.GetMask("Statics")))
+        {
+            radius = Mathf.Tan(lightSource.spotAngle*Mathf.Deg2Rad) * hit.distance;
+            center = hit.point;
+        }
+        
+        Collider[] colliders = Physics.OverlapSphere(center, radius / 2.5f, LayerMask.GetMask("Player"));
+        
+        if(colliders.Length > 0 )
+        {
+            PlayerCought();
+        }
+        else
+        {
+            PlayerGone();
+        }
+    }
+
+    void PlayerCought()
+    {
+        if (isCought) return;
+        isCought = true;
+        lightSource.DOColor(Color.red, 0.5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+        lightSource.spotAngle = yesSpotAngle;
+        rotator.StopRotate();
+    }
+
+    void PlayerGone()
+    {
+        if (!isCought) return;
+        isCought = false;
+        lightSource.DOKill();
+        lightSource.color = Color.white;
+        lightSource.spotAngle = noSpotAngle;
+        rotator.Rotate();
+    }
+}
