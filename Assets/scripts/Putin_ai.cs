6f297242a17d28a64bf15736c8ab136c3e1c7ef5
Erty8 <41906495+Erty8@users.noreply.github.com>
2021-01-10 12:21:55 +0300

    gun3_patch1

--- Assets/scripts/Putin_ai.cs
+++ Assets/scripts/Putin_ai.cs
@@ -6,14 +6,38 @@ public class Putin_ai : MonoBehaviour
 {
     public GameObject putinmodel;
     // Start is called before the first frame update
+    public float turnspeed = 1f;
+    public void turn()
+    {
+        //transform.RotateAround(transform.position, Vector3.up, 90*Time.deltaTime);
+        //transform.Rotate(new Vector3(0,90,0)*turnspeed);
+        //InvokeRepeating("turner", 1, 1);
+        while (transform.eulerAngles.y < 270)
+        {
+            transform.Rotate(Vector3.up * 1f * Time.deltaTime);
+        }
+        Debug.Log("turned");
+        void turner () { 
+            if (transform.eulerAngles.y < 270)
+            {
+                transform.Rotate(Vector3.up * 1 * Time.deltaTime);
+            } 
+        }
+        
+
+
+    }
     void Start()
     {
-        putinmodel.GetComponent<Animator>().Play("Putin_sit");
+        //turn();
+        //putinmodel.GetComponent<Animator>().Play("Putin_sit");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //
+        //turn();
         //putinmodel.GetComponent<Animator>().Play("Putin_sit");
     }
 }
